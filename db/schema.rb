f959e8558b51aea17b966f915b8e12166fdd64db
Chris Bohn <chrisbohn8@protonmail.com>
2018-06-12 13:30:08 -0500

    trigger heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_040704) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "beers", force: :cascade do |t|
-    t.string "name"
-    t.text "tagline"
-    t.string "abv"
-    t.string "tier"
-    t.text "description"
-    t.string "ibu"
-    t.string "image"
-    t.boolean "ontap"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
