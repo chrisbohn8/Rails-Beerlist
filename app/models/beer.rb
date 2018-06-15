163e5a9ae1af0b1414abd3a3c87b7c9f80863dcf
Chris Bohn <chrisbohn8@protonmail.com>
2018-06-15 13:45:56 -0500

    re migrate db

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,2 @@
 class Beer < ApplicationRecord
-	validates :name, presence: true
-	validates :tagline, presence: true
-	validates :abv, presence: true
-	validates :tier, presence: true
-	validates :image, presence: true
 end
